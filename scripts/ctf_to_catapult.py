6ef06882e4321232b4a39d13a2343a6a7882e75c
Enguerrand Decorne <decorne.en@gmail.com>
2019-09-30 11:12:49 +0200

    initial import

---
+++ scripts/ctf_to_catapult.py
@@ -0,0 +1,45 @@
+# Script to convert from OCaml CTF format to Chrome's catapult format, used in chrome://tracing
+import json
+import sys
+import babeltrace as bt
+
+def event_to_catapult(ev):
+    out = {}
+    out['ts'] = "{}.{:03d}".format(ev['timestamp'] // 1000, ev['timestamp'] % 1000)
+    out['pid'] = 0
+    if ev['id'] == 0:
+        out['ph'] = 'B'
+        out['name'] = ev['phase']
+    if ev['id'] == 1:
+        out['ph'] = 'E'
+        out['name'] = ev['phase']
+    if ev['id'] == 2:
+        out['args'] = {'value': ev['count']}
+        out['name'] = ev['kind']
+        out['ph'] = 'C'
+    if ev['id'] == 3:
+        out['args'] = {'value': ev['count']}
+        out['name'] = ev['bucket']
+        out['ph'] = 'C'
+    return out
+
+def main():
+    if len(sys.argv) != 3:
+        print("usage: ./ctf_to_catapult.py trace_directory out.json")
+        exit(0)
+    dir = sys.argv[1]
+    tr = bt.TraceCollection()
+    tr.add_trace(dir, 'ctf')
+    catapult_objects = list()
+    for event in tr.events:
+        if event['id'] <= 3:
+            catapult_objects.append(event_to_catapult(event))
+    catapult = {
+        'displayTimeUnit': 'ns',
+        'traceEvents': catapult_objects
+    }
+    with open(sys.argv[2], "w") as out:
+        out.write(json.dumps(catapult))
+
+if __name__ == '__main__':
+    main()
